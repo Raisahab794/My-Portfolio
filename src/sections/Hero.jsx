10c91a805f7890e5fa71bf84f3097a577808658a
Ayush <ayushkumarrai2@gmail.com>
2025-01-09 18:44:05 +0530

    initial commit

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,11 +1,72 @@
-import React from 'react'
+import React, { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { PerspectiveCamera } from '@react-three/drei'
+import HackerRoom from '../components/HackerRoom'
+import CanvasLoader from '../components/CanvasLoader'
+import { Leva, useControls } from 'leva'
 
 const Hero = () => {
+  const { positionX, positionY, positionZ, rotationX, rotationY, rotationZ, scale } = useControls('HackerRoom', {
+    positionX: {
+      value: 2.5,
+      min: -10,
+      max: 10,
+    },
+    positionY: {
+      value: 2.5,
+      min: -10,
+      max: 10,
+    },
+    positionZ: {
+      value: 2.5,
+      min: -10,
+      max: 10,
+    },
+    rotationX: {
+      value: 0,
+      min: -10,
+      max: 10,
+    },
+    rotationY: {
+      value: 0,
+      min: -10,
+      max: 10,
+    },
+    rotationZ: {
+      value: 0,
+      min: -10,
+      max: 10,
+    },
+    scale: {
+      value: 1,
+      min: 0.1,
+      max: 10,
+    },
+  })
+  
   return (
     <section className='min-h-screen w-full flex flex-col relative'>
-        <div className='w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3'>
-           <p>Hi, I am Ayush <span>&#128075;</span></p>
-        </div>
+      <div className='w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3'>
+        <p className='sm:text-3xl text-xl font-medium text-white text-center font-generalsans'>
+          Hi, I am Ayush <span className='waving-hand'>&#128075;</span>
+        </p>
+        <p className='hero_tag text-gray_gradient'>Building product & Brands</p>
+      </div>
+      <div className='w-full h-full absolute inset-0'>
+        <Canvas className='w-full h-full'>
+          <Suspense fallback={<CanvasLoader />}>
+            <PerspectiveCamera makeDefault position={[0, 0, 30]} />
+            <HackerRoom
+              scale={[0.1,0.1,0.1]}
+              position={[1,-9.5,4.1]}
+              rotation={[0.0,-3.2,0.0]}
+            />
+            <ambientLight intensity={1} />
+            <directionalLight intensity={0.5} position={[10, 10, 10]} />
+          </Suspense>
+        </Canvas>
+      </div>
+      <Leva />
     </section>
   )
 }
