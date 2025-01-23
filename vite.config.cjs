8d861f99397572f9a4a8e2c68c076a74a89a8614
Ayush <ayushkumarrai2@gmail.com>
2025-01-24 00:08:58 +0530

    Import path module in vite.config.cjs

--- vite.config.cjs
+++ vite.config.cjs
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path' // Add this line to import the path module
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
