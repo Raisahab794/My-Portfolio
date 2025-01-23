9cd23751ba13de34643e6fd47f99d3faaace0527
Ayush <ayushkumarrai2@gmail.com>
2025-01-23 23:55:27 +0530

    added

--- vite.config.cjs
+++ vite.config.cjs
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    postcss: './postcss.config.cjs',
-  },
+  build: {
+    chunkSizeWarningLimit: 1000, // Increase warning limit
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'three'],
+          utils: ['@react-three/fiber', '@react-three/drei']
+        }
+      }
+    }
+  }
 })
